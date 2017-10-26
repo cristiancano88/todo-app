079ce24ed2a004d8635f1449f5c79be825f9ddf7
cristiancano88 <cristiancano888@gmail.com>
2017-10-26 15:27:55 -0500

    código para la template del todo

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,5 +1,28 @@
 <template>
+  <li class="todo" :class="{'label-default': todo.done, editing: editing}">
+    <div>
+      <input type="checkbox"
+      class="toggle"
+      :checked="todo.none"
+      @change="toggleTodo({todo: todo})"/>
 
+      <label v-text="todo.text"
+      @dblclick="editing = true">
+      </label>
+
+      <button class="btn btn-xs btn-danger"
+      @click="deleteTodo({todo: todo})">
+        Eliminar
+      </button>
+    </div>
+
+    <input class="edit form-control"
+    v-show="editing"
+    :value="todo.text"
+    @keyup.enter="doneEdit"
+    @keyup.esc="cancelEdit"
+    @blur="doneEdit"/>
+  </li>
 </template>
 
 <script>
